d0bfa50acc8e1f95363260de7113c06925818524
Jesús Martínez <jesusmartinez@Jesuss-MacBook-Pro.local>
2018-07-04 19:55:36 -0500

    Adds remaining examples.

--- src/main/scala/ols/OrdinaryLeastSquares.scala
+++ src/main/scala/ols/OrdinaryLeastSquares.scala
@@ -1,5 +1,33 @@
 package ols
 
-object OrdinaryLeastSquares extends App {
+import smile.data.{Attribute, AttributeDataset, NominalAttribute, NumericAttribute}
+import smile.read
+import smile.validation.rmse
+import smile.regression.ols
 
+object OrdinaryLeastSquares extends App {
+  val attributes = new Array[Attribute](9)
+
+  attributes(0) = new NominalAttribute("vendor name")
+  attributes(1) = new NominalAttribute("Model Name")
+  attributes(2) = new NumericAttribute("MYCT")
+  attributes(3) = new NumericAttribute("MMIN")
+  attributes(4) = new NumericAttribute("MMAX")
+  attributes(5) = new NumericAttribute("CACH")
+  attributes(6) = new NumericAttribute("CHMIN")
+  attributes(7) = new NumericAttribute("CHMAX")
+  attributes(8) = new NumericAttribute("PRP")
+
+  val y = new NumericAttribute("ERP")
+
+  val dataFileUri = this.getClass.getClassLoader.getResource("lasso/machine.data").toURI.getPath
+  val data: AttributeDataset = read.csv(dataFileUri, attributes = attributes, response = Some((y, 9)))
+
+  println("Sneak peek of the data:")
+  println(data)
+
+  val model = ols(data.x(), data.y())
+  val predictions = model.predict(data.x())
+
+  println(s"Model's RMSE on the training set: ${rmse(data.y(), predictions)}")
 }
--- src/main/scala/rbfnet/RadialBasisFunctionNetwork.scala
+++ src/main/scala/rbfnet/RadialBasisFunctionNetwork.scala
@@ -1,5 +1,36 @@
 package rbfnet
+import smile.data.{Attribute, AttributeDataset, NumericAttribute}
+import smile.regression.rbfnet
+import smile.math.distance.EuclideanDistance
+import smile.read
+import smile.util.gaussrbf
+import smile.validation.rmse
 
 object RadialBasisFunctionNetwork extends App {
+  val attributes = new Array[Attribute](4)
+
+  attributes(0) = new NumericAttribute("AT")
+  attributes(1) = new NumericAttribute("V")
+  attributes(2) = new NumericAttribute("AP")
+  attributes(3) = new NumericAttribute("RH")
+
+  val y = new NumericAttribute("PE")
+
+  val dataFileUri = this.getClass.getClassLoader.getResource("rbfnet/Folds5x2_pp.csv").toURI.getPath
+  val data: AttributeDataset = read.csv(dataFileUri, attributes = attributes, response = Some((y, 4)), header = true)
+
+  println("Sneak peek of the data:")
+  println(data)
+
+  val centers = new Array[Array[Double]](50)
+  val basis = gaussrbf(data.x(), centers)
+  val model = rbfnet(
+    data.x(),
+    data.y(),
+    new EuclideanDistance,
+    basis,
+    centers)
+  val predictions = model.predict(data.x())
+  println(s"Model's RMSE on the training set: ${rmse(data.y(), predictions)}")
 
 }
